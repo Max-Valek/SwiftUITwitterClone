ba3d11d9e086b42a7587d95dcd08617a568e436e
Max Valek <valek.maxwell@gmail.com>
2023-04-14 13:28:12 -0500

    Initial Commit

---
+++ SwiftUITwitterClone/SwiftUITwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITwitterCloneApp.swift
+//  SwiftUITwitterClone
+//
+//  Created by Max Valek on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
