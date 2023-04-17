3ba1b5dd6e643021c8cc8e88afd529244ef6a84f
Max Valek <valek.maxwell@gmail.com>
2023-04-17 17:27:18 -0500

    modify bottom tab bar to look more like twitter

--- SwiftUITwitterClone/Scenes/Home/Following/FollowingView.swift
+++ SwiftUITwitterClone/Scenes/Home/Following/FollowingView.swift
@@ -14,7 +14,6 @@ struct FollowingView: View {
         ScrollView {
             ForEach(0..<10, id: \.self) { num in
                 TweetView()
-                    .foregroundColor(.blue)
                 
                 RoundedRectangle(cornerRadius: 5)
                     .frame(height: 0.5)
