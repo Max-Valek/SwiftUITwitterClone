5b034f82bbc894ef56cc99f4f2781e340e32cb8a
Max Valek <valek.maxwell@gmail.com>
2023-04-28 12:03:00 -0500

    add location, date joined, following, and followers to profile view

--- SwiftUITwitterClone/Core/Scenes/Profile/ProfileView.swift
+++ SwiftUITwitterClone/Core/Scenes/Profile/ProfileView.swift
@@ -16,69 +16,49 @@ struct ProfileView: View {
             
             VStack(spacing: 0) {
                 
-                // header + buttons
-                VStack {
-                    ZStack(alignment: .top) {
-                        Rectangle()
-                            .fill(Color.theme.twitter)
-                            // .frame(height: 140)
-                            .frame(maxWidth: .infinity)
-                            .ignoresSafeArea()
+                headerAndButtons
+                
+                photoAndEditProfile
+                
+                nameAndUsername
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    HStack(spacing: 16) {
+                        HStack(spacing: 2) {
+                            Image(systemName: "location.north.circle")
+                            Text("Location")
+                        }
                         
-                        VStack {
-                            HStack {
-                                Image(systemName: "arrow.left")
-                                
-                                Spacer()
-                                
-                                Image(systemName: "magnifyingglass")
-                            }
-                            .padding()
+                        HStack(spacing: 2) {
+                            Image(systemName: "calendar")
+                            Text("Joined April 2023")
+                        }
+                        Spacer()
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(Color.theme.text.opacity(0.65))
+                    
+                    HStack(spacing: 16) {
+                        HStack(spacing: 2) {
+                            Text("100")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                            Text("Following")
+                                .font(.subheadline)
+                                .foregroundColor(Color.theme.text.opacity(0.65))
+                        }
+                        HStack(spacing: 2) {
+                            Text("100")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                            Text("Followers")
+                                .font(.subheadline)
+                                .foregroundColor(Color.theme.text.opacity(0.65))
                         }
                     }
                     
                 }
-                .frame(height: 80)
-                
-                HStack(alignment: .center) {
-                    Image(systemName: "person")
-                        .resizable()
-                        .frame(width: 40, height: 40)
-                        .padding()
-                        .background(Color.black)
-                        .clipShape(Circle())
-                        .offset(y: -20)
-                        
-                    Spacer()
-                    
-                    Button {
-                        // edit profile pressed
-                    } label: {
-                        Text("Edit profile")
-                            .font(.subheadline)
-                            .padding(4)
-                            .padding(.horizontal, 4)
-                            .background(RoundedRectangle(cornerRadius: 20).stroke(Color.theme.text, lineWidth: 1))
-                        
-                    }
-                }
-                .padding(.horizontal)
-                
-                
-                HStack {
-                    
-                    VStack(alignment: .leading) {
-                        Text("User")
-                            .font(.title2)
-                            .fontWeight(.black)
-                        Text("@user")
-                            .font(.headline)
-                    }
-                    
-                    Spacer()
-                }
                 .padding(.horizontal, 25)
-                .offset(y: -10)
                 
                 HStack {
                     Text("tabs")
@@ -102,5 +82,78 @@ struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
             .preferredColorScheme(.dark)
+    }
+}
+
+extension ProfileView {
+    
+    private var headerAndButtons: some View {
+        VStack {
+            ZStack(alignment: .top) {
+                Rectangle()
+                    .fill(Color.theme.twitter)
+                    // .frame(height: 140)
+                    .frame(maxWidth: .infinity)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    HStack {
+                        Image(systemName: "arrow.left")
+                        
+                        Spacer()
+                        
+                        Image(systemName: "magnifyingglass")
+                    }
+                    .padding()
+                }
+            }
+            
+        }
+        .frame(height: 80)
+    }
+    
+    private var photoAndEditProfile: some View {
+        HStack(alignment: .center) {
+            Image(systemName: "person")
+                .resizable()
+                .frame(width: 40, height: 40)
+                .padding()
+                .background(Color.black)
+                .clipShape(Circle())
+                .offset(y: -20)
+                
+            Spacer()
+            
+            Button {
+                // edit profile pressed
+            } label: {
+                Text("Edit profile")
+                    .font(.subheadline)
+                    .padding(4)
+                    .padding(.horizontal, 4)
+                    .background(RoundedRectangle(cornerRadius: 20).stroke(Color.theme.text, lineWidth: 1))
+                
+            }
+        }
+        .padding(.horizontal)
+    }
+    
+    private var nameAndUsername: some View {
+        HStack {
+            
+            VStack(alignment: .leading) {
+                Text("User")
+                    .font(.title2)
+                    .fontWeight(.black)
+                Text("@user")
+                    .font(.headline)
+                    .foregroundColor(Color.theme.text.opacity(0.5))
+            }
+            
+            Spacer()
+        }
+        .padding(.horizontal, 25)
+        .padding(.bottom, -10)
+        .offset(y: -20)
     }
 }
