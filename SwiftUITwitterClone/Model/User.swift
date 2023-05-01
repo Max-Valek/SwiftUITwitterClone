9dfdbfc1042709d9fab304b6c95676fafaae9980
Max Valek <valek.maxwell@gmail.com>
2023-05-01 16:55:14 -0500

    add more sample users with profile photos

--- SwiftUITwitterClone/Model/User.swift
+++ SwiftUITwitterClone/Model/User.swift
@@ -11,7 +11,7 @@ struct User: Identifiable {
     var id = UUID()
     var displayName: String
     var username: String
-    var profilePhoto: Image?
+    var profilePhoto: String?
     var location: String?
     // let dateJoined: String
     var following: Int
@@ -19,9 +19,4 @@ struct User: Identifiable {
     var tweets: [Tweet]?
     var replies: [Tweet]?
     var likedPosts: [Tweet]?
-}
-
-extension User {
-    
-    static var dogeUser = User(displayName: "Doge", username: "@doge", location: "Dogeland", following: 1, followers: 100, tweets: Tweet.userTweets, replies: nil, likedPosts: nil)
 }
